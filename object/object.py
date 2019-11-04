65874305cb85174ac49f0eb9d64fd9fae4a97db6
Kaappo <kaappo.raivio@gmail.com>
2019-11-04 10:56:09 +0200

    asd
    Took 4 minutes

--- object/object.py
+++ object/object.py
@@ -1,13 +1,19 @@
 from typing import Tuple
-import physics
+from object import physics
+
+from object.sprite import Sprite
 
 
 class Object:
     def __init__(self, path_to_sprite):
-        self._x = -1, self._y = -1
+        self._x = -1
+        self._y = -1
+
+        self.sprite = Sprite.fromFile(path_to_sprite)
+        self.physics = physics.Physics()
 
     def updateAndMove(self, deltaTime: float) -> None:
-        self._x, self._y = physics.getNewPosition()
+        self._x, self._y = self.physics.getNewPosition()
 
     @property
     def pos(self) -> Tuple[int, int]:
