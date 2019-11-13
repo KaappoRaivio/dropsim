315a25670050f5f88ec2bd14d268d1d803632bac
Into Almiala <into.almiala@gmail.com>
2019-11-13 13:27:53 +0200

    final commit v1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 if __name__ != "__main__":
     raise Exception()
@@ -12,12 +13,18 @@ our_scene = scene.Scene(screen)
 
 # muffincup = object.Object("assets/muffincup.png", reverse=True)  # TODO
 # Untitled = object.Object("assets/Untitled.png", reverse=False)  # TODO
-ball = object.Object("assets/ball.png", reverse=True, pos=(-0.02, 50), air_resistance=False)  # TODO
-ball2 = object.Object("assets/ball.png", reverse=True, pos=(0.01, 50), C=1)  # TODO
+# ball = object.Object("assets/ball.png", reverse=True, pos=(-0.02, 50), air_resistance=False)  # TODO
+# ball2 = object.Object("assets/ball.png", reverse=True, pos=(0.01, 50), C=1)  # TODO
 # while True:
 #     print(o.sprite.mass, o.sprite.area, o.pos)
 #     o.updateAndMove(0.01)
 #     time.sleep(0.01)
-our_scene.addObjects((ball, ball2))
+
+objects = []
+
+with open(sys.argv[1], 'r') as file:
+    objects = list(map(eval, file.readlines()))
+
+our_scene.addObjects(objects)
 our_scene.render()
 our_scene.runForEver()
