22db958643bc066c14db3e2bcd8b09bf9edfb82c
Kaappo <kaappo.raivio@gmail.com>
2019-11-13 13:13:04 +0200

    autocommit
    Took 6 minutes

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from object import object
 screen = graphics.Graphics()
 our_scene = scene.Scene(screen)
 
-ball = object.Object("assets/ball.png", reverse=True, pos=(-0.02, 50), air_resistance=False)  # TODO
-ball2 = object.Object("assets/ball.png", reverse=True, pos=(0.01, 50), C=1)  # TODO
+ball = object.Object("assets/ball.png", reverse=True, pos=(-0.02, 10), air_resistance=False)  # TODO
+ball2 = object.Object("assets/ball.png", reverse=True, pos=(0.01, 10), C=1)  # TODO
 
 our_scene.addObjects((ball, ball2))
 our_scene.render()
